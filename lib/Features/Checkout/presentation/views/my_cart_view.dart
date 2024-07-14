c2c0284b6540979400cab195224091555a7ea95a
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-14 21:09:23 +0300

    create app styles

--- lib/Features/Checkout/presentation/views/my_cart_view.dart
+++ lib/Features/Checkout/presentation/views/my_cart_view.dart
@@ -1,3 +1,4 @@
+import 'package:checkout_app/Core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class MyCartView extends StatelessWidget {
@@ -5,6 +6,22 @@ class MyCartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Cart',
+          textAlign: TextAlign.center,
+          style: Styles.style25,
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.black,
+            size: 32,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
