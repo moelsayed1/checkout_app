3d3cb28e83133d88178c11b2fc82f03f79da5175
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-21 22:59:29 +0300

    create checkout cubit

---
+++ lib/Features/Checkout/presentation/manager/cubits/stripe_payment_cubit/stripe_payment_cubit.dart
@@ -0,0 +1,34 @@
+import 'dart:developer';
+import 'package:checkout_app/Features/Checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
+import 'package:checkout_app/Features/Checkout/data/repos/checkout_repo.dart';
+import 'package:checkout_app/Features/Checkout/presentation/manager/cubits/stripe_payment_cubit/stripe_payment_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class StripePaymentCubit extends Cubit<StripePaymentState> {
+  StripePaymentCubit(this.checkoutRepo) : super(StripePaymentInitial());
+
+  final CheckoutRepo checkoutRepo;
+
+  Future makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(StripePaymentLoading());
+
+    var data = await checkoutRepo.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+
+    data.fold(
+      (failure) => emit(
+        StripePaymentFailure(failure.errMessage),
+      ),
+      (success) => emit(
+        StripePaymentSuccess(),
+      ),
+    );
+  }
+
+  @override
+  void onChange(Change<StripePaymentState> change) {
+    log(change.toString());
+    super.onChange(change);
+  }
+}
