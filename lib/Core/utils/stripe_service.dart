2b89a4412695f15622fd969455818d0ca7e0bfb5
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-23 06:28:11 +0300

    create customer request

--- lib/Core/utils/stripe_service.dart
+++ lib/Core/utils/stripe_service.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:checkout_app/Core/utils/api_keys.dart';
 import 'package:checkout_app/Core/utils/api_service.dart';
+import 'package:checkout_app/Features/Checkout/data/models/customer_payment_intent_model/customer_payment_intent.dart';
 import 'package:checkout_app/Features/Checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
 import 'package:checkout_app/Features/Checkout/data/models/payment_intent_model/payment_intent.dart';
 import 'package:dio/dio.dart';
@@ -50,18 +51,18 @@ class StripeService {
     await displayPaymentSheet();
   }
 
-  Future<PaymentIntentModel> createCustomerPaymentIntent(
-      PaymentIntentInputModel paymentIntentInputModel) async {
+  Future<CustomerPaymentIntentModel> createCustomerPaymentIntent(
+      CustomerPaymentIntentModel customerIntentModel) async {
 
 
     var response = await apiService.post(
-      body: paymentIntentInputModel.toJson(),
+      body: customerIntentModel.toJson(),
       contentType: Headers.formUrlEncodedContentType,
-      url:'https://api.stripe.com/v1/payment_intents',
+      url:'https://api.stripe.com/v1/customers',
       token: ApiKeys.secretKey,
     );
     log("${response.statusCode}Res");
-    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
-    return paymentIntentModel;
+    var customerPaymentIntent = CustomerPaymentIntentModel.fromJson(response.data);
+    return customerPaymentIntent;
   }
 }
